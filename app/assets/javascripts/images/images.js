fa452be2992d49033593527ad6031acea53d6b6d
Andur Carr <carr.andur@gmail.com>
2014-11-22 14:46:53 -0800

    Add tagging function

--- app/assets/javascripts/images/images.js
+++ app/assets/javascripts/images/images.js
@@ -17,6 +17,12 @@ $(document).ready(function(){
       $('.tag').hide();
     });
 
+    $('#images-show').on('click', 'input', function(e){
+      e.preventDefault();
+      var newTag = WW.Tagging.addTag($(e.target).prev().val(), WW.Targeting.currentPosition());
+      WW.Tagging.showTag(newTag);
+    });
+
   }
 });
 
@@ -24,18 +30,23 @@ $(document).ready(function(){
 var WW = WW || {};
 WW.Targeting = (function(){
 
-  _boxWidth = 100;
-  _boxHeight = 100;
+  var _boxWidth = 100;
+  var _boxHeight = 100;
+  var position = {};
 
   function _getCoords(context, event){
     var offset = $(context).offset();
     var relativeX = (event.pageX - offset.left);
     var relativeY = (event.pageY - offset.top);
-    return {x: relativeX, y: relativeY}
+    return {x: relativeX, y: relativeY};
+  }
+
+  function currentPosition(){
+    return position;
   }
 
   function targetBox(context, event){
-    var position = _getCoords(context, event);
+    position = _getCoords(context, event);
 
     // don't redraw box if interacting with its children
     if ($(event.target).parents().hasClass('target-box')){
@@ -45,35 +56,77 @@ WW.Targeting = (function(){
 
     //create an absolutely positioned div around relativeX, relativeY
     $('.target-box').remove();
-    $box = $('<div class = "target-box"></div>');
+    var $box = $('<div class = "target-box"></div>');
 
     $box.css({
       height: _boxHeight + "px",
       width: _boxWidth + "px",
       top: (position.y - _boxHeight / 2 ) + "px",
-      left: (position.x - _boxWidth / 2 ) + "px" 
+      left: (position.x - _boxWidth / 2 ) + "px"
     });
     $(context).append($box);
 
 
-    $box.append('<select name = "characters"><option value = "waldo">Waldo</option></select>');
     //that div has a dropdown: populate that dropdown with selection options for characters
+    $box.append('<select name = "characters"><option value = "Waldo">Waldo</option></select>');
+    $box.append('<input value="Tag me!" type="submit"></input>');
 
     //selecting a character creates a Tag associated with that character at that location
 
     // alert('X: ' + position.x + ', ' + 'Y: ' + position.y);
 
-  
+
   }
 
 
 
-  return { targetBox: targetBox };
+  return { targetBox: targetBox,
+            currentPosition: currentPosition };
 
 })();
 
 
 
 WW.Tagging = (function(){
-  return {};
+
+
+  var tags = [];
+  var _tagHeight = 100;
+  var _tagWidth = 100;
+
+  function addTag(character, position){
+    var newTag = new Tag(character, position);
+    tags.push(newTag);
+    return newTag;
+  }
+
+  function Tag(character, position){
+    this.position = position;
+    this.character = character;
+  }
+
+  function showTag(tag){
+    //create an absolutely positioned div around relativeX, relativeY
+    $('.target-box').remove();
+    var $tag = $('<div class = "tag"></div>');
+
+    $tag.css({
+      height: _tagHeight + "px",
+      width: _tagWidth + "px",
+      top: (tag.position.y - _tagHeight / 2 ) + "px",
+      left: (tag.position.x - _tagWidth / 2 ) + "px"
+    });
+    $('#images-show').append($tag);
+
+    $tag.append('<p>' + tag.character + '</p>');
+
+
+  }
+
+  return { Tag: Tag,
+          tags: tags,
+          addTag: addTag,
+          showTag: showTag };
+
 }());
+
