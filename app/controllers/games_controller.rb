e53824c5cddcbc37a357ca47a577c42b6aff7177
Michael Alexander <alexandermw@gmail.com>
2014-11-23 17:57:09 -0800

    all the epics

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,6 +12,7 @@ class GamesController < ApplicationController
   def edit
     @game = Game.find(params[:id])
     @game.update(:completed_at => Time.now)
+    @game.update_score
     respond_to do |format|
        format.js { render :edit, status: 200, location: @game }
        format.html {redirect_to @game, notice: "You won!!" }
@@ -19,8 +20,25 @@ class GamesController < ApplicationController
   end
 
   def update
+    @game = Game.find(params[:id])
+    @high_scores = Game.high_scores
+    respond_to do |format|
+      if @game.update(game_params)
+        format.js { render :update, status: 200, location: @game }
+        format.html {redirect_to @game, notice: "Big Winner!!" }
+      else
+        format.html { render :edit }
+        format.js { render json: @game.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:player_name)
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -7,4 +7,20 @@ class Game < ActiveRecord::Base
     completed_at - created_at
   end
 
+  def update_score
+    time = (1800 - victory_time).round
+    self.score = (time > 0) ? time : 0
+    self.save
+  end
+
+  def is_high_score?
+    Game.high_scores.pluck(:score).include?(self.score)
+  end
+
+  def self.high_scores
+    Game.order(score: :desc).limit(10).select(:player_name, :score)
+  end
+
+
+
 end
